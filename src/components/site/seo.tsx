fb3ca9053a09fc3bebbbfbd2e7628e5c297183e5
umiio <umiiomail@gmail.com>
2026-08-24 14:47:40 +0300

    1ثحديث

--- src/components/site/seo.tsx
+++ src/components/site/seo.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from "react";
 
 // قم بتحديث رابط الموقع الجديد هنا
-const SITE_URL = "https://www.sanadak-ae.com";
+const SITE_URL = "https://uae-complaits.com/";
 const DEFAULT_OG_IMAGE = `${SITE_URL}/og-image.jpg`;
 
 interface SeoProps {
